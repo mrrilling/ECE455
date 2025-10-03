afa06297b010f6e7b490d7291674ffdf9f3cb9c3
mrrilling <mrrilling@wisc.edu>
2025-10-03 16:16:36 -0500

    first commit

--- HW04/p5.sh
+++ HW04/p5.sh
@@ -6,7 +6,7 @@
 #SBATCH --gpus-per-task=1
 #SBATCH --time=00:01:00
 #SBATCH --output=vector_add_streams.output
-cd $SLURM_SUBMIT_DIRls
+cd $SLURM_SUBMIT_DIR
 module load nvidia/cuda
 nvcc vector_add_streams.cu -o vector_add_streams
 ./vector_add_streams
