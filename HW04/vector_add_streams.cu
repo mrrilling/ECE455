3f8e652fbff12b201aa5870d984358c9ac994227
mrrilling <mrrilling@wisc.edu>
2025-10-03 15:45:19 -0500

    first commit

---
+++ HW04/vector_add_streams.cu
@@ -0,0 +1,54 @@
+#include <stdio.h>
+__global__ void vector_add(const float *A, const float *B, float *C, int N)
+{
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if (i < N)
+        C[i] = A[i] + B[i];
+}
+int main()
+{
+    int N = 1000000;
+    size_t size = N * sizeof(float);
+    float *A, *B, *C;
+    float *d_A, *d_B, *d_C;
+    A = (float *)malloc(size);
+    B = (float *)malloc(size);
+    C = (float *)malloc(size);
+    for (int i = 0; i < N; i++)
+    {
+        A[i] = 1 .0 f;
+        B[i] = 2 .0 f;
+    }
+    cudaMalloc(&d_A, size);
+    cudaMalloc(&d_B, size);
+    cudaMalloc(&d_C, size);
+    cudaStream_t stream 1, stream 2;
+    cudaStreamCreate(&stream 1);
+    cudaStreamCreate(&stream 2);
+    int half = N / 2;
+    size_t half_size = size / 2;
+    cudaMemcpyAsync(d_A, A, half_size, cudaMemcpyHostToDevice, stream 1);
+    cudaMemcpyAsync(d_B, B, half_size, cudaMemcpyHostToDevice, stream 1);
+    cudaMemcpyAsync(d_A + half, A + half, half_size,
+                    cudaMemcpyHostToDevice, stream 2);
+    cudaMemcpyAsync(d_B + half, B + half, half_size,
+                    cudaMemcpyHostToDevice, stream 2);
+    int threads = 2 5 6;
+    int blocks_half = (half + threads - 1) / threads;
+    vector_add<<<blocks_half, threads, 0, stream 1>>>(d_A, d_B, d_C, half);
+    vector_add<<<blocks_half, threads, 0, stream 2>>>(d_A+half, d_B+half, d_ +half, half);
+    cudaMemcpyAsync(C, d_C, half_size, cudaMemcpyDeviceToHost, stream 1);
+    cudaMemcpyAsync(C + half, d_C + half, half_size,cudaMemcpyDeviceToHost, stream 2);
+    cudaStreamSyncronize(stream 1);
+    cudaStreamSyncronize(stream 2);
+    printf(" C [0] = %f , C [N -1] = %f\n ", C[0], C[N - 1]);
+    cudaStreamDestroy(stream 1);
+    cudaStreamDestroy(stream 2);
+    cudaFree(d_A);
+    cudaFree(d_B);
+    cudaFree(d_C);
+    free(A);
+    free(B);
+    free(C);
+    return 0;
+}
